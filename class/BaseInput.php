3493d90bde26c879d55a42003cc3e79764bbb8f6
Abdullah Almamun <mamun2social@gmail.com>
2024-01-16 11:20:06 +0600

    Updated BaseInput

--- class/BaseInput.php
+++ class/BaseInput.php
@@ -4,12 +4,11 @@ class BaseInput{
     protected string $name;
     protected string $label;
     protected string $value;
+
     public function __construct(string $name, string $label = '', string $value = '')
     {
         $this->name = $name;
         $this->label = $label;
         $this->value = $value;
     }
-
-
 }
